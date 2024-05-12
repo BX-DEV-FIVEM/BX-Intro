dab44443be840333acc58b231ea9b7b8b729bf2b
BX-DEV <140925178+BX-DEV-FIVEM@users.noreply.github.com>
2024-05-12 11:58:52 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -32,3 +32,52 @@ AddEventHandler('bx_giveStarterPack', function()
         end
     end
 end)
+
+
+
+RegisterServerEvent('bx:checkintro')
+AddEventHandler('bx:checkintro', function()
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+
+    if xPlayer then
+        local identifier = xPlayer.identifier
+        MySQL.Async.fetchScalar('SELECT intro_statut FROM users WHERE identifier = @identifier', {
+            ['@identifier'] = identifier
+        }, function(starterPackGiven)
+            starterPackGiven = tonumber(starterPackGiven)
+
+            if starterPackGiven == 0 then
+                MySQL.Async.execute('UPDATE users SET intro_statut = 1 WHERE identifier = @identifier', {
+                    ['@identifier'] = identifier
+                })
+                TriggerClientEvent('bx:startintro', _source)
+            else
+
+            end
+        end)
+    end
+end)
+
+
+RegisterNetEvent('bx:setPlayerBucket')
+AddEventHandler('bx:setPlayerBucket', function(source, bucketId)
+    SetPlayerRoutingBucket(source, bucketId)
+    
+end)
+
+RegisterNetEvent('bx:restorePlayerBucket')
+AddEventHandler('bx:restorePlayerBucket', function(source)
+    SetPlayerRoutingBucket(source, 0)
+end)
+
+
+RegisterNetEvent('bx:setEntityBucket')
+AddEventHandler('bx:setEntityBucket', function(entityId, bucketId)
+    SetEntityRoutingBucket(entity, 1)
+end)
+
+AddEventHandler('playerDropped', function(reason)
+    local src = source
+    SetPlayerRoutingBucket(src, 0)
+end)
